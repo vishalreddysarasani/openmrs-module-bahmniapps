43690423a4bdf90c49bfb787f0646c61e71eeee3
padma <padmavb@thoughtworks.com>
2016-07-13 16:09:17 +0530

    Padma | Adding search for auto complete multi select observation in the configured conceptset answers

--- ui/app/common/concept-set/controller/multiSelectObservationController.js
+++ ui/app/common/concept-set/controller/multiSelectObservationController.js
@@ -1,19 +1,35 @@
 'use strict';
 
-angular.module('bahmni.common.conceptSet').controller('multiSelectObservationSearchController',['$scope', function($scope){
+angular.module('bahmni.common.conceptSet').controller('multiSelectObservationSearchController', ['$scope', 'conceptSetService', function ($scope, conceptSetService) {
 
-    var possibleAnswers = $scope.observation.getPossibleAnswers();
+    var possibleAnswers = [];
+    var unselectedValues = [];
     $scope.values = [];
 
-    var init = function(){
-        _.values($scope.observation.selectedObs).forEach(function(observation){
-            $scope.values.push({"label" : observation.autocompleteValue, "name" : observation.autocompleteValue});
+    var init = function () {
+        var selectedValues = _.map(_.values($scope.observation.selectedObs),'value');
+        selectedValues.forEach(function (observation) {
+            $scope.values.push({"label": observation.name,"name" : observation.name});
         });
+
+        var configuredConceptSetName = $scope.observation.getConceptUIConfig().answersConceptName;
+        if (!_.isUndefined(configuredConceptSetName)) {
+            conceptSetService.getConcept({
+                name: configuredConceptSetName,
+                v: "bahmni"
+            }).then(function (response) {
+                possibleAnswers = _.isEmpty(response.data.results) ? [] : response.data.results[0].answers;
+                unselectedValues = _.xorBy(possibleAnswers, $scope.values, 'uuid');
+            });
+        } else {
+            possibleAnswers = $scope.observation.getPossibleAnswers();
+            unselectedValues = _.xorBy(possibleAnswers, selectedValues, 'uuid');
+        }
     };
 
-    $scope.search = function(query){
+    $scope.search = function (query) {
         var matchingAnswers = new Array();
-        _.forEach(possibleAnswers, function(answer){
+        _.forEach(unselectedValues, function (answer) {
             if (typeof answer.name != "object" && answer.name.toLowerCase().indexOf(query.toLowerCase()) !== -1) {
                 answer.label = answer.name;
                 matchingAnswers.push(answer);
@@ -34,17 +50,29 @@ angular.module('bahmni.common.conceptSet').controller('multiSelectObservationSea
             }
 
         });
-        return _.uniqBy(matchingAnswers,'uuid');
+        return _.uniqBy(matchingAnswers, 'uuid');
     };
 
-    $scope.setLabel = function(answer){
+    $scope.addItem = function (item) {
+        unselectedValues = _.remove(unselectedValues, function (value) {
+            return value.uuid !== item.uuid;
+        });
+        $scope.observation.toggleSelection(item);
+    };
+
+    $scope.removeItem = function (item) {
+        unselectedValues.push(item);
+        $scope.observation.toggleSelection(item);
+    };
+
+    $scope.setLabel = function (answer) {
         answer.label = answer.name;
         return true;
     };
 
-    $scope.validate=function() {
+    $scope.validate = function () {
         var value = $("input.input").val();
-        if(_.isEmpty($scope.search(value))){
+        if (_.isEmpty($scope.search(value))) {
             $("input.input").val("");
             $($('tags-input')[0]).removeClass('ng-invalid');
         }
